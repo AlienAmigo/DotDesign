60dbb63d4ff91694bbc92576daa859adb5e7d464
Pete <maestro_pete@inbox.ru>
2019-10-31 16:22:53 +0300

    прикрутил owl-carousel

--- config.js
+++ config.js
@@ -23,12 +23,16 @@ let config = {
     // 'src/scss/print.scss',
   ],
   'addJsBefore': [
+    // 'src/js/owl.carousel.min.js'
     // 'somePackage/dist/somePackage.js', // для 'node_modules/somePackage/dist/somePackage.js',
   ],
   'addJsAfter': [
     './script.js',
   ],
   'addAssets': {
+    'src/js/jquery-3.4.1.min.js': 'js/',
+    'src/blocks/owl-carousel/code.js': 'js/',
+    'src/blocks/owl-carousel/owl.carousel.min.js': 'js/',
     'src/fonts/demo-empty-open-sans.woff2': 'fonts/',
     'src/img/demo-*.{png,svg,jpg,jpeg}': 'img/',
     // 'src/favicon/*.{png,ico,svg,xml,webmanifest}': 'img/favicon',
---
+++ src/blocks/owl-carousel/code.js
@@ -0,0 +1,16 @@
+// console.log('You are awesome');
+$(document).ready(function(){
+  $(".owl-slider-1").owlCarousel({
+    loop: true,
+    // nav: true,
+    items: 1,
+  });
+});
+
+$(document).ready(function(){
+  $(".owl-slider-2").owlCarousel({
+    loop: true,
+    // nav: true,
+    items: 3,
+  });
+});
